2efa40279ba6010d2760b4a6b3ec909f54157036
cmiltiadou <cmiltiad@gmail.com>
2021-10-26 19:28:56 -0400

    added code to recordSequence to switch keypresses to opposite key for the final round

--- js/app.js
+++ js/app.js
@@ -20,19 +20,6 @@ const resetArray = () => {
     console.log('arrays reset')
 }
 
-// let compareArrays = function (arrP1, arrP2) {
-//     let i = arrP1.length
-   
-//     if (i !== arrP2.length) 
-//         return false
-    
-//     while (i--) {
-//         if (arrP1[i] !== arrP2[i])
-//             return false
-//     }
-//     return true
-
-//         }
 
 const checkForWin = () => {
     // let compareArrays = function () {
@@ -43,7 +30,8 @@ const checkForWin = () => {
     //     } 
     // }
     let arraysEqual = ""
-    compareArrays()
+
+    // compareArrays()
         switch(true) {
             case(currentRd =="1b"):
                 if(arrP1.sort().join(',') === arrP2.sort().join(',')){
@@ -95,7 +83,7 @@ let timeleft = 6;
         
     
     const recordSequence = (e) => {
-        if(currentArray.length < 4){
+        if(currentArray.length < 4 && currentRd !== "3b"){
     switch(e.key){
         //if the w key is pressed, append to 
         case ('w'):
@@ -110,7 +98,24 @@ let timeleft = 6;
         case ('d'):
             currentArray.push('D')
             break
-    }}else {
+    }} else if(currentArray.length < 4 && currentRd !== "3b"){
+        switch(e.key){
+            //for the final rd, we'll have p2's keypresses correspond to the opposite key (i.e. pressing W = S) 
+            case ('w'):
+                currentArray.push('S')
+                break
+            case ('a'):
+                currentArray.push('D')
+                break
+            case ('s'):
+                currentArray.push('W')
+                break
+            case ('d'):
+                currentArray.push('A')
+                break
+        }
+
+    } else {
         return false
     }
     checkArr.addEventListener('click', console.log(currentArray))  
@@ -124,40 +129,47 @@ let timeleft = 6;
 // Player 2 now has to match Player 1's sequence exactly as entered // try to compare on the fly OR
 // store player 2's array and compare to player 1 and check for win or lose conditions using if else statement
 //
+const countdown = function(){
+    if(timeleft <= 0 || currentArray.length == 4){
+        clearInterval(countdown);
+        timer.innerText = ""
+        playerStatus.innerText = "Player 2, follow their lead!"
+        setTimeout((currentArray = arrP2), 10000)
+        currentRd = "1b"
+        // setTimeout(checkForWin, 2000)
+        // console.log("array 2 " + arrP2)
+        // console.log("array 1 " + arrP1)  
+        // add button so Player 2 can enter a matching sequence
+        // if(arrP2.length == 4 && arrP1.length == 4 ){
+        //     checkForWin()
+        // setTimeout(resetArray, 1000)
+        timer.innerText = "Player 1 get ready"
+        
+                startButton.value = "Proceed"
+                startButton.style.display = "inherit"
+                console.log("this is array 2 " +arrP2)
+                console.log("this is array 1 " +arrP1)
+                } else {
+                    timer.innerText = timeleft 
+                        timeleft -= 1;} }
+        
 
 const startGame = () => {
     if (currentRd != 0) { document.addEventListener('keydown', recordSequence)
     }
     startButton.style.display = "none"
-    // setTimeout(resetArray, 1000)
-    timer.innerText = "Player 1 get ready"
-    
-    const countdown = setInterval(function(){
-        if(timeleft <= 0 || currentArray.length == 4){
-            clearInterval(countdown);
-            timer.style.fontSize = "30px"
-            timer.innerText = ""
-            playerStatus.innerText = "Player 2, follow their lead!"
-            setTimeout((currentArray = arrP2), 10000)
-            currentRd = "1b"
-            // setTimeout(checkForWin, 2000)
-            // console.log("array 2 " + arrP2)
-            // console.log("array 1 " + arrP1)  
-            // add button so Player 2 can enter a matching sequence
-            if(arrP2.length == 4 && arrP1.length == 4 ){
-                checkForWin()
 
-            }
-            startButton.value = "Proceed"
-            startButton.style.display = "inherit"
-            console.log("this is array 2 " +arrP2)
-            console.log("this is array 1 " +arrP1)
-            } else {
-                timer.innerText = timeleft 
-                timeleft -= 1;}
-    }, 1040)
+    switch (true){
+        case (currentRd == "1"):
+            setInterval(() => {
+                countdown()                
+                
+            }, 1040);
+            break
 
-    
-}
+
+}}
+
+
 
 startButton.addEventListener('click', startGame)
